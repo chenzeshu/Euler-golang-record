2ddd869c2543ab6c6eb539090100a651db33818a
chenzeshu <1193297950@qq.com>
2017-09-29 13:14:07 +0800

    10 sieve

--- 10.go
+++ 10.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func getPrime(n int64) []int64 {
+	var result = make([]int64, 0, n/int64( math.Log(float64(n) )))
+	var sieve = make([]bool, n+1)
+	var sn = int64( math.Sqrt(float64(n)) )
+	var i, j int64
+	for i = 2; i<= sn; i++{
+		if !sieve[i] {
+			for j = i+i ; j<=n; j += i{
+				sieve[j] = true
+			}
+		}
+	}
+
+	for i = 2; i<=n ; i++{
+		if !sieve[i] {
+			result = append(result, i)
+		}
+	}
+
+	return result
+}
+
+func main()  {
+	primes := getPrime(2e6)
+	var sum int64 = 0
+	for _, k:= range primes{
+		sum += k
+	}
+	fmt.Println(sum)
+}
